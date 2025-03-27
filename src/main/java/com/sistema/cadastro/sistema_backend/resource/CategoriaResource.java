19b0d9756c081b3ccc42ccc27d94b2361c9d8cac
Walisson Martins da Silva <walisson.silva@implantainformatica.com.br>
2025-03-27 20:14:49 -0300

    feat: Rest Controller Get ArrayList

--- src/main/java/com/sistema/cadastro/sistema_backend/resource/CategoriaResource.java
+++ src/main/java/com/sistema/cadastro/sistema_backend/resource/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.sistema.cadastro.sistema_backend.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sistema.cadastro.sistema_backend.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REst works";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Desktop");
+        Categoria cat2 = new Categoria(2, "Informatica");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
