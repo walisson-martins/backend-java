c4367dbcde1114723e3f87e9219aefc2d827e4b0
Walisson Martins da Silva <walisson.silva@implantainformatica.com.br>
2025-04-11 18:45:40 -0300

    feat: Created Client and Address Entity

--- src/main/java/com/sistema/cadastro/sistema_backend/SistemaBackendApplication.java
+++ src/main/java/com/sistema/cadastro/sistema_backend/SistemaBackendApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sistema.cadastro.sistema_backend.domain.Categoria;
 import com.sistema.cadastro.sistema_backend.domain.Cidade;
+import com.sistema.cadastro.sistema_backend.domain.Cliente;
+import com.sistema.cadastro.sistema_backend.domain.Endereco;
 import com.sistema.cadastro.sistema_backend.domain.Estado;
 import com.sistema.cadastro.sistema_backend.domain.Produto;
+import com.sistema.cadastro.sistema_backend.enums.TipoCliente;
 import com.sistema.cadastro.sistema_backend.repositories.CategoriaRepository;
 import com.sistema.cadastro.sistema_backend.repositories.CidadeRepository;
+import com.sistema.cadastro.sistema_backend.repositories.ClienteRepository;
+import com.sistema.cadastro.sistema_backend.repositories.EnderecoRepository;
 import com.sistema.cadastro.sistema_backend.repositories.EstadoRepository;
 import com.sistema.cadastro.sistema_backend.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SistemaBackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 
@@ -52,6 +63,14 @@ public class SistemaBackendApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "5615484212", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2765655332", "56455124"));
+
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "APTO 303", "Jardim", "3822034", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "45", "APTO 4", "Jardim", "3823234", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -66,6 +85,8 @@ public class SistemaBackendApplication implements CommandLineRunner {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
