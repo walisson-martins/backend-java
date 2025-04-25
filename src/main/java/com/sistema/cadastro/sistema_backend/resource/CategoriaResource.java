6ce04b48631029ad97a28c0308552c9da66211ba
Walisson Martins da Silva <walisson.silva@implantainformatica.com.br>
2025-04-24 21:02:53 -0300

    feat: Created method Post Category

--- src/main/java/com/sistema/cadastro/sistema_backend/resource/CategoriaResource.java
+++ src/main/java/com/sistema/cadastro/sistema_backend/resource/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.sistema.cadastro.sistema_backend.resource;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sistema.cadastro.sistema_backend.domain.Categoria;
 import com.sistema.cadastro.sistema_backend.services.CategoriaService;
@@ -27,4 +30,12 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
 }
--- src/main/java/com/sistema/cadastro/sistema_backend/services/CategoriaService.java
+++ src/main/java/com/sistema/cadastro/sistema_backend/services/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
         return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException(
             "Objeto não encontrado! :Id " + id + ", Tipo: " + Categoria.class.getName())));
     }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
